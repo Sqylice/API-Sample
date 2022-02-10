30d00aad4a64d3881f58479a5b4f35609f0d7685
iyshdfiuygsd <oiauidubsd@jhbifbaids.se>
2022-02-10 21:58:16 +0100

    Staged unstaged

---
+++ EmployeeAPI/src/components/Employees.vue
@@ -0,0 +1,19 @@
+<template>
+  
+</template>
+
+<script>
+import EmployeeData from 'http://dummy.restapiexample.com/api/v1/employees'
+
+export default {
+    data() {
+        return{
+            EmployeeData
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ EmployeeAPI/src/components/Loop.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <div v-for="Employee in EmployeeData" v-bind:key="Employee.id">
+          <h3>{{ Employee.employee_name }}</h3>
+          <p>{{ Employee.employee_salary }}</p>
+          <p>{{ Employee.employee_age }}</p>
+          <!-- <img src="{{ EmployeeData.employee_img }}" alt=""> -->
+      </div>
+  </div>
+</template>
+
+<script>
+import EmployeeData from 'http://dummy.restapiexample.com/api/v1/employees.json'
+
+export default {
+    // data() {
+    //     return {
+
+    //     }
+    // }
+}
+</script>
+
+<style>
+
+</style>
