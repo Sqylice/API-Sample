3eff218a215a5f46e34144beab2c7535e659fa22
iyshdfiuygsd <oiauidubsd@jhbifbaids.se>
2022-02-11 14:10:38 +0100

    Fix after I accidentally deleted the remote solution instead of the local one

--- EmployeeAPI/src/components/Loop.vue
+++ EmployeeAPI/src/components/Loop.vue
@@ -3,6 +3,8 @@
     <div>
         <div v-for="Employee in EmployeeData" v-bind:key="Employee.id">
             <div class="card" style="width: 15rem;">
+                <!-- As no images are in the file, this isn't strictly necessary but it's nice to have -->
+                <!-- <img class="card-img-top" src="{{ Employee.profile_image }}" alt="A profile picture of the employee"> -->
                 <div class="card-body bg-dark">
                     <h5 class="card-title">{{ Employee.employee_name }}</h5>
                     <p class="card-text">Salary: {{ Employee.employee_salary }}</p>
@@ -14,17 +16,20 @@
 </template>
 
 <script>
-//Pull the local .json file and check that it works
-    import json from "@/data/employees.json"
-    console.log(json.status);
+import axios from 'axios'
 
 export default {
-    data() {
-        return {
-            // We only care about the data section for our reader
-            EmployeeData: json.data
-        }
+    data () {
+    return {
+      EmployeeData: null
     }
+  },
+  mounted () {
+    axios
+      .get('http://dummy.restapiexample.com/api/v1/employees')
+      .then(response => (this.EmployeeData = response.data))
+
+  }
 }
 </script>
 
