edf1e786c8855051e7673097dd491ea5ae753a36
iyshdfiuygsd <oiauidubsd@jhbifbaids.se>
2022-02-11 14:56:43 +0100

    There, fixed the syntax with an extra ".data"

--- EmployeeAPI/src/components/Loop.vue
+++ EmployeeAPI/src/components/Loop.vue
@@ -15,8 +15,8 @@
     <div v-else>
         <div v-for="Employee in EmployeeBackUpData" v-bind:key="Employee.id">
             <div class="card" style="width: 15rem;">
-                <!-- As no images are in the file, this isn't strictly necessary but it's nice to have -->
-                <!-- <img class="card-img-top" src="{{ Employee.profile_image }}" alt="A profile picture of the employee"> -->
+                <!-- As no images are in the file, this isn't strictly necessary but it's nice to have
+                <img class="card-img-top" src="{{ Employee.profile_image }}" alt="A profile picture of the employee"> -->
                 <div class="card-body bg-dark">
                     <h5 class="card-title">{{ Employee.employee_name }}</h5>
                     <p class="card-text">Salary: {{ Employee.employee_salary }}</p>
@@ -45,11 +45,8 @@ export default {
   mounted () {
     axios
         .get('http://dummy.restapiexample.com/api/v1/employees')
-        .then(response => (this.EmployeeData = response.data))
+        .then(response => (this.EmployeeData = response.data.data))
+        console.log(this.EmployeeData)
   }
 }
 </script>
-
-<style>
-
-</style>
