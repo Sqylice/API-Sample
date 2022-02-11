8df5b391f98b63dfadc42adb87a111657ad31a81
iyshdfiuygsd <oiauidubsd@jhbifbaids.se>
2022-02-11 12:09:22 +0100

    Cleaning up some chaff

--- EmployeeAPI/src/App.vue
+++ EmployeeAPI/src/App.vue
@@ -1,18 +1,14 @@
 <script setup>
-// import Navbar from './components/Navbar.vue'
 // import Employees from './components/Employees.vue';
-import Employees_local from './components/Employees_local.vue';
 import Loop from './components/Loop.vue';
 </script>
 
 <template>
   <div>
     <header class="wrapper">
-        <Navbar />
     </header>
     <body>
       <!-- <Employees /> -->
-      <!-- <Employees_local /> -->
       <Loop />
     </body>
   </div>
